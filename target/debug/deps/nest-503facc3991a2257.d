6d337bfc56d8481cbad9bd0d420221ff8522d700
AmidVoshakul <amid.voshakul@gmail.com>
2026-04-16 14:12:35 +0300

    Remove accidentally committed .opencode folder

---
+++ target/debug/deps/nest-503facc3991a2257.d
@@ -0,0 +1,12 @@
+/home/amid/Develop/nest/target/debug/deps/nest-503facc3991a2257.d: src/main.rs Cargo.toml
+
+/home/amid/Develop/nest/target/debug/deps/libnest-503facc3991a2257.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CARGO_PKG_DESCRIPTION=Secure hypervisor for autonomous AI agents
+# env-dep:CARGO_PKG_NAME=nest
+# env-dep:CARGO_PKG_VERSION=0.1.0
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/nest_permissions-f6029d4cc3017f74.d
@@ -0,0 +1,7 @@
+/home/amid/Develop/nest/target/debug/deps/nest_permissions-f6029d4cc3017f74.d: crates/nest-permissions/src/lib.rs
+
+/home/amid/Develop/nest/target/debug/deps/libnest_permissions-f6029d4cc3017f74.rmeta: crates/nest-permissions/src/lib.rs
+
+crates/nest-permissions/src/lib.rs:
+
+# env-dep:linux=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__--no-deps__CLIPPY_HACKERY__
